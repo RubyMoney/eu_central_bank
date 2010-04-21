da40ef411673585f76021625e1523b4f959b19ad
Wong Liang Zan <zan@liangzan.net>
2010-04-21 08:05:21 +0800

    specs for eu central bank

--- spec/eu_central_bank_spec.rb
+++ spec/eu_central_bank_spec.rb
@@ -1,7 +1,48 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "EuCentralBank" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:each) do
+    @bank = EuCentralBank.new
+    @cache_path = File.expand_path(File.dirname(__FILE__) + '/exchange_rates.xml')
+    @yml_cache_path = File.expand_path(File.dirname(__FILE__) + '/exchange_rates.yml')
+    @tmp_cache_path = File.expand_path(File.dirname(__FILE__) + '/tmp/exchange_rates.xml')
+  end
+
+  after(:each) do
+    if File.exists? @tmp_cache_path
+      File.delete @tmp_cache_path
+    end
+  end
+
+  it "should save the xml file from ecb given a file path" do
+    @bank.save_rates(@tmp_cache_path)
+    File.exists?(@tmp_cache_path).should == true
+  end
+
+  it "should raise an error if an invalid path is given to save_rates" do
+    lambda { @bank.save_rates(nil) }.should raise_exception
+  end
+
+  it "should update itself with exchange rates from ecb website" do
+    stub(OpenURI::OpenRead).open(EuCentralBank::ECB_RATES_URL) {@cache_path}
+    @bank.update_rates
+    EuCentralBank::CURRENCIES.each do |currency|
+      @bank.get_rate("EUR", currency).should > 0
+    end
+  end
+
+  it "should update itself with exchange rates from cache" do
+    @bank.update_rates(@cache_path)
+    EuCentralBank::CURRENCIES.each do |currency|
+      @bank.get_rate("EUR", currency).should > 0
+    end
+  end
+
+  it "should return the correct exchange rates" do
+    EXCHANGE_RATES = YAML.load_file(@yml_cache_path)
+    @bank.update_rates(@cache_path)
+    EuCentralBank::CURRENCIES.each do |currency|
+      @bank.get_rate("EUR", currency).should == EXCHANGE_RATES["currencies"][currency]
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'eu_central_bank'
 require 'spec'
 require 'spec/autorun'
+require 'shoulda'
+require 'rr'
 
 Spec::Runner.configure do |config|
-  
+  config.mock_with :rr
 end

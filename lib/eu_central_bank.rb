fd23ab99e28a90f19f89644de92e7efff0ebdda0
Crawford Wynnes <crawfordwynnes@gmail.com>
2014-05-08 10:02:20 +0100

    Get FX rates with a date
    Added ability to calculate from historical rates
    
    Fix exchange with historical data and refactor with calculate exchange method
    
    Added tests to check when the historical cache was updated
    
    fixes for rdoc and readability improvemens
    
    modified historical exchange rates yaml to use BigDecimal
    
    use hashrocket syntax

--- lib/eu_central_bank.rb
+++ lib/eu_central_bank.rb
@@ -9,19 +9,26 @@ class EuCentralBank < Money::Bank::VariableExchange
 
   attr_accessor :last_updated
   attr_accessor :rates_updated_at
+  attr_accessor :historical_last_updated
+  attr_accessor :historical_rates_updated_at
 
-  ECB_RATES_URL = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
   CURRENCIES = %w(USD JPY BGN CZK DKK GBP HUF ILS LTL PLN RON SEK CHF NOK HRK RUB TRY AUD BRL CAD CNY HKD IDR INR KRW MXN MYR NZD PHP SGD THB ZAR)
+  ECB_RATES_URL = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
+  ECB_90_DAY_URL = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
 
   def update_rates(cache=nil)
     update_parsed_rates(doc(cache))
   end
 
-  def save_rates(cache)
-    raise InvalidCache if !cache
+  def update_historical_rates(cache=nil)
+    update_parsed_historical_rates(doc(cache, ECB_90_DAY_URL))
+  end
+
+  def save_rates(cache, url=ECB_RATES_URL)
+    raise InvalidCache unless cache
     File.open(cache, "w") do |file|
-      io = open(ECB_RATES_URL) ;
-      io.each_line {|line| file.puts line}
+      io = open(url);
+      io.each_line { |line| file.puts line }
     end
   end
 
@@ -29,34 +36,57 @@ class EuCentralBank < Money::Bank::VariableExchange
     update_parsed_rates(doc_from_s(content))
   end
 
-  def save_rates_to_s
-    open(ECB_RATES_URL).read
+  def save_rates_to_s(url=ECB_RATES_URL)
+    open(url).read
   end
 
-  def exchange(cents, from_currency, to_currency)
-    exchange_with(Money.new(cents, from_currency), to_currency)
+  def exchange(cents, from_currency, to_currency, date=nil)
+    exchange_with(Money.new(cents, from_currency), to_currency, date)
   end
 
-  def exchange_with(from, to_currency)
-    rate = get_rate(from.currency, to_currency)
+  def exchange_with(from, to_currency, date=nil)
+    from_base_rate, to_base_rate = nil, nil
+    rate = get_rate(from, to_currency, {:date => date})
+
     unless rate
-      from_base_rate, to_base_rate = nil, nil
-      @mutex.synchronize {
-        from_base_rate = get_rate("EUR", from.currency, :without_mutex => true)
-        to_base_rate = get_rate("EUR", to_currency, :without_mutex => true)
-      }
+      @mutex.synchronize do
+        opts = { :date => date, :without_mutex => true }
+        from_base_rate = get_rate("EUR", from.currency.to_s, opts)
+        to_base_rate = get_rate("EUR", to_currency, opts)
+      end
       rate = to_base_rate / from_base_rate
     end
-    Money.new(((BigDecimal(Money::Currency.wrap(to_currency).subunit_to_unit) / BigDecimal(from.currency.subunit_to_unit)) * from.cents * rate).round, to_currency)
+
+    calculate_exchange(from, to_currency, rate)
+  end
+
+  def get_rate(from, to, opts = {})
+    fn = -> { @rates[rate_key_for(from, to, opts)] }
+
+    if opts[:without_mutex]
+      fn.call
+    else
+      @mutex.synchronize { fn.call }
+    end
+  end
+
+  def set_rate(from, to, rate, opts = {})
+    fn = -> { @rates[rate_key_for(from, to, opts)] = rate }
+
+    if opts[:without_mutex]
+      fn.call
+    else
+      @mutex.synchronize { fn.call }
+    end
   end
 
   protected
 
-  def doc(cache)
-    rates_source = !!cache ? cache : ECB_RATES_URL
-    Nokogiri::XML(open(rates_source)).tap {|doc| doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube') }
+  def doc(cache, url=ECB_RATES_URL)
+    rates_source = !!cache ? cache : url
+    Nokogiri::XML(open(rates_source)).tap { |doc| doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube') }
   rescue Nokogiri::XML::XPath::SyntaxError
-    Nokogiri::XML(open(ECB_RATES_URL))
+    Nokogiri::XML(open(url))
   end
 
   def doc_from_s(content)
@@ -66,7 +96,7 @@ class EuCentralBank < Money::Bank::VariableExchange
   def update_parsed_rates(doc)
     rates = doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
 
-    @mutex.synchronize do 
+    @mutex.synchronize do
       rates.each do |exchange_rate|
         rate = BigDecimal(exchange_rate.attribute("rate").value)
         currency = exchange_rate.attribute("currency").value
@@ -79,5 +109,41 @@ class EuCentralBank < Money::Bank::VariableExchange
     @rates_updated_at = Time.parse(rates_updated_at)
 
     @last_updated = Time.now
+  end
+
+  def update_parsed_historical_rates(doc)
+    rates = doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
+
+    @mutex.synchronize do
+      rates.each do |exchange_rate|
+        rate = BigDecimal(exchange_rate.attribute("rate").value)
+        currency = exchange_rate.attribute("currency").value
+        opts = { :without_mutex => true }
+        opts[:date] = exchange_rate.parent.attribute("time").value
+        set_rate("EUR", currency, rate, opts)
+        set_rate("EUR", "EUR", 1, opts)
+      end
+    end
+
+    rates_updated_at = doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube/@time').first.value
+    @historical_rates_updated_at = Time.parse(rates_updated_at)
+
+    @historical_last_updated = Time.now
+  end
+
+  private
+
+  def calculate_exchange(from, to_currency, rate)
+    to_currency_money = Money::Currency.wrap(to_currency).subunit_to_unit
+    from_currency_money = from.currency.subunit_to_unit
+    decimal_money = BigDecimal(to_currency_money) / BigDecimal(from_currency_money)
+    money = (decimal_money * from.cents * rate).round
+    Money.new(money, to_currency)
+  end
+
+  def rate_key_for(from, to, opts)
+    key = "#{from}_TO_#{to}"
+    key << "_#{opts[:date].to_s}" if opts[:date]
+    key.upcase
   end
 end

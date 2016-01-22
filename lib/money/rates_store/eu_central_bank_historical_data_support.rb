f95a7281d61299ae62bf006fceab1f82399473cb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2016-01-22 15:32:55 +0200

    Fixed thread safety issue
    Fixed issue with thread safety when the rates are read from the bank's store object.

--- lib/money/rates_store/eu_central_bank_historical_data_support.rb
+++ lib/money/rates_store/eu_central_bank_historical_data_support.rb
@@ -24,7 +24,9 @@ module Money::RatesStore
     #   end
     def each_rate(&block)
       enum = Enumerator.new do |yielder|
-        index.each do |key, rate|
+        # Duplicate index here so that it is not changed while we are reading
+        # the values from it.
+        index.dup.each do |key, rate|
           iso_from, iso_to = key.split(Memory::INDEX_KEY_SEPARATOR)
           iso_to, date = iso_to.split("_")
           date = Date.parse(date) if date

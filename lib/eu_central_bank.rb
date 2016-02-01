RubyMoney/eu_central_bank

commit 94051d36f9ce1dd4655ec858606da3ef7b32f497

Thread safety fixing + refactoring
Trying to fix the thread safety issue by ensuring a new mutex synchronize block can be initiated when needed.

Also changed the date separator in the rates index, now defined by the INDEX_DATE_SEPARATOR constant.

lib/eu_central_bank.rb
@@ -126,7 +126,7 @@ class EuCentralBank < Money::Bank::VariableExchange
   def update_parsed_rates(doc)
     rates = doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
 
-    store.transaction do
+    store.transaction true do
       rates.each do |exchange_rate|
         rate = BigDecimal(exchange_rate.attribute("rate").value)
         currency = exchange_rate.attribute("currency").value
@@ -144,7 +144,7 @@ class EuCentralBank < Money::Bank::VariableExchange
   def update_parsed_historical_rates(doc)
     rates = doc.xpath('gesmes:Envelope/xmlns:Cube/xmlns:Cube//xmlns:Cube')
 
-    store.transaction do
+    store.transaction true do
       rates.each do |exchange_rate|
         rate = BigDecimal(exchange_rate.attribute("rate").value)
         currency = exchange_rate.attribute("currency").value

lib/money/rates_store/eu_central_bank_historical_data_support.rb
@@ -1,5 +1,6 @@
 module Money::RatesStore
   module EuCentralBankHistoricalDataSupport
+    INDEX_DATE_SEPARATOR = '_AT_'.freeze
 
     def add_rate(currency_iso_from, currency_iso_to, rate, date = nil)
       transaction { index[rate_key_for(currency_iso_from, currency_iso_to, date)] = rate }
@@ -8,6 +9,21 @@ module Money::RatesStore
     def get_rate(currency_iso_from, currency_iso_to, date = nil)
       transaction { index[rate_key_for(currency_iso_from, currency_iso_to, date)] }
     end
+
+  # Wraps block execution in a thread-safe transaction
+  def transaction(force_sync = false, &block)
+    force_sync = false if @mutex.locked? && @mutex.owned?
+    if !force_sync && (@in_transaction || options[:without_mutex])
+      block.call self
+    else
+      @mutex.synchronize do
+        @in_transaction = true
+        result = block.call
+        @in_transaction = false
+        result
+      end
+    end
+  end
 
     # Iterate over rate tuples (iso_from, iso_to, rate)
     #
@@ -19,14 +35,14 @@ module Money::RatesStore
     # @return [Enumerator]
     #
     # @example
-    #   store.each_rate do |iso_from, iso_to, rate|
-    #     puts [iso_from, iso_to, rate].join
+    #   store.each_rate do |iso_from, iso_to, rate, date|
+    #     puts [iso_from, iso_to, rate, date].join
     #   end
     def each_rate(&block)
       enum = Enumerator.new do |yielder|
         index.each do |key, rate|
           iso_from, iso_to = key.split(Memory::INDEX_KEY_SEPARATOR)
-          iso_to, date = iso_to.split("_")
+          iso_to, date = iso_to.split(INDEX_DATE_SEPARATOR)
           date = Date.parse(date) if date
           yielder.yield iso_from, iso_to, rate, date
         end
@@ -39,7 +55,7 @@ module Money::RatesStore
 
       def rate_key_for(currency_iso_from, currency_iso_to, date = nil)
         key = [currency_iso_from, currency_iso_to].join(Memory::INDEX_KEY_SEPARATOR)
-        key << "_#{date.to_s}" if date
+        key = [key, date.to_s].join(INDEX_DATE_SEPARATOR) if date
         key.upcase
       end
 

commit 8a6d5060989238f2154599e841a08e4b5fc3547a

More thread safety fixes
More efforts on fixing the thread safety related issues.

Also, added backwards compatibility for Ruby 1.9.3 which was previously broken by 94051d36f9ce1dd4655ec858606da3ef7b32f497.

lib/eu_central_bank.rb
@@ -86,11 +86,33 @@ class EuCentralBank < Money::Bank::VariableExchange
     end
   end
 
+  def export_rates(format, file = nil, opts = {})
+    raise Money::Bank::UnknownRateFormat unless
+      RATE_FORMATS.include? format
+
+    store.transaction true do
+      s = case format
+      when :json
+        JSON.dump(rates)
+      when :ruby
+        Marshal.dump(rates)
+      when :yaml
+        YAML.dump(rates)
+      end
+
+      unless file.nil?
+        File.open(file, "w") {|f| f.write(s) }
+      end
+
+      s
+    end
+  end
+
   def import_rates(format, s, opts = {})
     raise Money::Bank::UnknownRateFormat unless
       RATE_FORMATS.include? format
 
-    store.transaction do
+    store.transaction true do
       data = case format
        when :json
          JSON.load(s)
@@ -102,8 +124,7 @@ class EuCentralBank < Money::Bank::VariableExchange
 
       data.each do |key, rate|
         from, to = key.split(SERIALIZER_SEPARATOR)
-        to, date = to.split("_")
-        store.add_rate from, to, rate, date
+        store.add_rate from, to, rate
       end
     end
 

lib/money/rates_store/eu_central_bank_historical_data_support.rb
@@ -12,7 +12,14 @@ module Money::RatesStore
 
   # Wraps block execution in a thread-safe transaction
   def transaction(force_sync = false, &block)
-    force_sync = false if @mutex.locked? && @mutex.owned?
+    # Ruby 1.9.3 does not support @mutex.owned?
+    if @mutex.respond_to?(:owned?)
+      force_sync = false if @mutex.locked? && @mutex.owned?
+    else
+      # If we allowed this in Ruby 1.9.3, it might possibly cause recursive
+      # locking within the same thread.
+      force_sync = false
+    end
     if !force_sync && (@in_transaction || options[:without_mutex])
       block.call self
     else
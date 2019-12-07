eadb4825e87576e1c6f4c1b8089b6b7c5fd9b101
Anthony Dmitriyev <antstorm@gmail.com>
2019-12-07 13:08:50 +0000

    Use thread-safety provided by the superclass in StoreWithHistoricalDataSupport

--- lib/money/rates_store/store_with_historical_data_support.rb
+++ lib/money/rates_store/store_with_historical_data_support.rb
@@ -3,33 +3,16 @@ module Money::RatesStore
     INDEX_DATE_SEPARATOR = '_AT_'.freeze
 
     def add_rate(currency_iso_from, currency_iso_to, rate, date = nil)
-      transaction { index[rate_key_for(currency_iso_from, currency_iso_to, date)] = rate }
+      transaction { rates[rate_key_for(currency_iso_from, currency_iso_to, date)] = rate }
     end
 
     def get_rate(currency_iso_from, currency_iso_to, date = nil)
-      transaction { index[rate_key_for(currency_iso_from, currency_iso_to, date)] }
+      transaction { rates[rate_key_for(currency_iso_from, currency_iso_to, date)] }
     end
 
     # Wraps block execution in a thread-safe transaction
-    def transaction(force_sync = false, &block)
-      # Ruby 1.9.3 does not support @mutex.owned?
-      if @mutex.respond_to?(:owned?)
-        force_sync = false if @mutex.locked? && @mutex.owned?
-      else
-        # If we allowed this in Ruby 1.9.3, it might possibly cause recursive
-        # locking within the same thread.
-        force_sync = false
-      end
-      if !force_sync && (@in_transaction || options[:without_mutex])
-        block.call self
-      else
-        @mutex.synchronize do
-          @in_transaction = true
-          result = block.call
-          @in_transaction = false
-          result
-        end
-      end
+    def transaction(_force_sync = false, &block)
+      super &block
     end
 
     # Iterate over rate tuples (iso_from, iso_to, rate)
@@ -47,7 +30,7 @@ module Money::RatesStore
     #   end
     def each_rate(&block)
       enum = Enumerator.new do |yielder|
-        index.each do |key, rate|
+        rates.each do |key, rate|
           iso_from, iso_to = key.split(Memory::INDEX_KEY_SEPARATOR)
           iso_to, date = iso_to.split(INDEX_DATE_SEPARATOR)
           date = Date.parse(date) if date
@@ -60,10 +43,10 @@ module Money::RatesStore
 
     private
 
-      def rate_key_for(currency_iso_from, currency_iso_to, date = nil)
-        key = [currency_iso_from, currency_iso_to].join(Memory::INDEX_KEY_SEPARATOR)
-        key = [key, date.to_s].join(INDEX_DATE_SEPARATOR) if date
-        key.upcase
-      end
+    def rate_key_for(currency_iso_from, currency_iso_to, date = nil)
+      key = [currency_iso_from, currency_iso_to].join(Memory::INDEX_KEY_SEPARATOR)
+      key = [key, date.to_s].join(INDEX_DATE_SEPARATOR) if date
+      key.upcase
+    end
   end
 end

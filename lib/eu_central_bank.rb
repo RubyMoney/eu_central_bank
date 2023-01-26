149102cf19712b1000b7382287cd3953ed96d535
Peter Goldstein <peter.m.goldstein@gmail.com>
2023-01-25 20:09:03 -0500

    Move the Croatian kuna (HRK) and Russian ruble (RUB) to legacy currencies.

--- lib/eu_central_bank.rb
+++ lib/eu_central_bank.rb
@@ -17,12 +17,12 @@ class EuCentralBank < Money::Bank::VariableExchange
 
   SERIALIZER_DATE_SEPARATOR = '_AT_'
   DECIMAL_PRECISION = 5
-  CURRENCIES = %w(USD JPY BGN CZK DKK GBP HUF ILS ISK PLN RON SEK CHF NOK HRK RUB TRY AUD BRL CAD CNY HKD IDR INR KRW MXN MYR NZD PHP SGD THB ZAR).map(&:freeze).freeze
+  CURRENCIES = %w(USD JPY BGN CZK DKK GBP HUF ILS ISK PLN RON SEK CHF NOK TRY AUD BRL CAD CNY HKD IDR INR KRW MXN MYR NZD PHP SGD THB ZAR).map(&:freeze).freeze
   ECB_RATES_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'.freeze
   ECB_90_DAY_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'.freeze
   ECB_ALL_HIST_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml'.freeze
 
-  LEGACY_CURRENCIES = %w(CYP SIT ROL TRL)
+  LEGACY_CURRENCIES = %w(CYP HRK RUB SIT ROL TRL)
 
   def initialize(st = Money::RatesStore::StoreWithHistoricalDataSupport.new, &block)
     super
